0592fbb97c71bb600151a7009b84b2f1a0c7f701
nboidoku <niiakoboi-doku@Niis-MacBook-Pro.local>
2017-06-24 17:01:34 -0400

    mongo connection string

--- connect/app.js
+++ connect/app.js
@@ -6,11 +6,10 @@ mongoose.Promise = require('q').Promise;
 var connectionString = 'mongodb://localhost/webdev'; // for local
 
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    console.log('here');
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds161487.mlab.com:61487/heroku_qqmcl53h'; // user yours
+    connectionString += '@ds051665.mlab.com:51665/heroku_096kzdbd'; // user yours
 }
 
 mongoose.connect(connectionString);
